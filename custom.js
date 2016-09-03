c1433cea76260e61b97105a1f2dad7dcdeb225d9
BSVogler <e-mail@benediktvogler.de>
2016-09-03 21:44:44 +0200

    added animation

--- custom.js
+++ custom.js
@@ -3,13 +3,16 @@ scaling = 20;
 sceneOffset = new THREE.Vector3(1,1,0);
 showSpeed = false;
 showDir = false;
+currentAnimStep=0;
 
 
-document.getElementById("cursor").addEventListener("animationbegin", function(){ alert("Hello World!"); });
+//document.getElementById("cursoranim").addEventListener("animationbegin", function(){ alert("Hello World!"); });
+//register animation end to add new position
+document.getElementById("cursor").addEventListener("animationend", nextAnimationStep);
 
 function Datapoint(){
-	this.timestamp = 0.0;
-	this.position = new THREE.Vector3();
+	this.timestamp = 0.0; //time in ms
+	this.position = new THREE.Vector3();//in m(?)
 	this.distanceToNext = Number.POSITIVE_INFINITY;
 }
 
@@ -104,161 +107,176 @@ function addTargetsToDOM(){
 	color: color of line
 */
 function addTrialToDOM(taskId, trialId, color) {
-
-	//when file is loaded, parse it
-	function finishedLoading(text) {
-		lines = text.split('\n');
-		var lastDP = null;
-		var maxDistance = 0;
-		var maxDt = 0;
-		var maxDv = 0;
-
-		var duplicatesCount = 0;
-		for (var i = 0; i < lines.length; i++) {
-			var line = lines[i];
-			if (line.startsWith("t:")) {
-				//create new datapoint with timestamp and matrix
-				dp = new Datapoint();
-				dp.timestamp = parseFloat(line.substring(2, line.length));
-				i += 1; //skip next line
-				var matrix = new THREE.Matrix4();
-				matrix.set(
-					parseFloat(lines[i].substring(1, 6)),
-					parseFloat(lines[i].substring(7, 12)),
-					parseFloat(lines[i].substring(13, 18)),
-					parseFloat(lines[i].substring(19, 24)),
-					parseFloat(lines[i + 1].substring(1, 6)),
-					parseFloat(lines[i + 1].substring(7, 12)),
-					parseFloat(lines[i + 1].substring(13, 18)),
-					parseFloat(lines[i + 1].substring(19, 24)),
-					parseFloat(lines[i + 2].substring(1, 6)),
-					parseFloat(lines[i + 2].substring(7, 12)),
-					parseFloat(lines[i + 2].substring(13, 18)),
-					parseFloat(lines[i + 2].substring(19, 24)),
-					parseFloat(lines[i + 3].substring(1, 6)),
-					parseFloat(lines[i + 3].substring(7, 12)),
-					parseFloat(lines[i + 3].substring(13, 18)),
-					parseFloat(lines[i + 3].substring(19, 24))
-				);
-
-
-				//var quaternion = new THREE.Quaternion();
-				//var scale = new THREE.Vector3();
-				//matrix.decompose( dp.position, quaternion, scale );
-
-				dp.position = new THREE.Vector3(1, 1, 1).applyMatrix4(matrix);
-				//shift data to view, then add to path
-				//dp.position.applyQuaternion(quaternion);
-				dp.position.multiplyScalar(scaling);
-				dp.position.add(sceneOffset);
-
-				//current one is duplicate
-				if (listOfDPs.length > 0 && dp.position.equals(listOfDPs[listOfDPs.length-1].position))  {
-					duplicatesCount++;
-					//current one is the last one
-					listOfDPs[listOfDPs.length-1].timestamp = dp.timestamp;//refrehs timestap to last know timestamp at this pos
-					dp = listOfDPs[listOfDPs.length-1];
-				} else {
-					listOfDPs.push(dp);
-				}
-				
-				//calcualte distance of last DP to the current one
-				if (showSpeed && lastDP != null) {
-					lastDP.distanceToNext = lastDP.position.distanceTo(dp.position);
-					var dt = dp.timestamp - lastDP.timestamp;
-					var dv = lastDP.distanceToNext / dt;
-					if (lastDP.distanceToNext > maxDistance)
-						maxDistance = lastDP.distanceToNext;
-					if (dt > maxDt)
-						maxDt = dt;
-					if (dv > maxDv)
-						maxDv = dv;
-				}
-				lastDP = dp;
-				
-				i += 4; //skip rest
-			}
-		}
-
-		console.log("Done parsing to "+ listOfDPs.length + " elements. Max dv:"+maxDv+" maxDistance: "+maxDistance+" max dt: "+ maxDt+"s. Removed "+duplicatesCount+" duplicate points.");
-		
-		//add to DOM
-		if (!showSpeed){
-			//add one big line
-			var lineDOMObject = document.createElement("a-entity");
-			lineDOMObject.setAttribute("class",taskId+"-"+trialId)
-			lineDOMObject.setAttribute("line","color:"+color+";")
-			var path = new Array();
-		}
-		
-		var up = new THREE.Vector3(0,1,0);
-		
-		for (var i = 0; i < listOfDPs.length; i++) {
-			if (showSpeed) {
-				if (i < listOfDPs.length-1) {
-					var line = document.createElement("a-entity");
-					line.setAttribute("class", taskId+"-"+trialId)
-				
-					//var hexBrightness = new Buffer(1/distance, 'hex')[0];
-					var dt = listOfDPs[i+1].timestamp - listOfDPs[i].timestamp;
-					var dv = listOfDPs[i].distanceToNext / dt;
-					var hexBrightness = (parseInt(255*dv / maxDv)).toString(16);
-					line.setAttribute("line", "color:#"+hexBrightness+hexBrightness+hexBrightness+";");
-					if (dt > 2*maxDt / 3){
-						//var hexBrightness = (parseInt(255*dv/maxDv)).toString(16);
-						line.setAttribute("line","color:#ff0000;");
-					}
-					line.setAttribute("line", "path", [listOfDPs[i].position,listOfDPs[i+1].position]);
-					scene.appendChild(line);
-				}
-			} else {
-				path.push(listOfDPs[i].position);
-			}
-			
-			//add cones for direction
-			if (showDir && i % 3 == 0 && i < listOfDPs.length-1) { //skip some elements
-				//add cone for direction
-				var cone = document.createElement("a-cone");
-				//cone.setAttribute("geometry","primitive","cone");
-				cone.setAttribute("class", taskId+"-"+trialId)
-				cone.setAttribute("color", color);
-				cone.setAttribute("radius-bottom", 0.008);
-				cone.setAttribute("radius-top", 0);
-				cone.setAttribute("open-ended", false);
-				cone.setAttribute("geometry", "segmentsRadial", 4);
-				cone.setAttribute("geometry", "segmentsHeight", 4);
-				cone.setAttribute("height", 0.04);
-				cone.setAttribute("position", listOfDPs[i].position);
-
-				var norm = new THREE.Vector3().copy(listOfDPs[i + 1].position).sub(listOfDPs[i].position).normalize();
-				//var norm = new THREE.Vector3(1,1,0).normalize();
-				var euler = new THREE.Euler(0, 0, 0, 'XYZ');
-				euler.setFromQuaternion(new THREE.Quaternion().setFromUnitVectors ( up, norm));
-			
-				cone.setAttribute("rotation", (euler.x * 180 / Math.PI) + " " + (euler.y * 180 / Math.PI) + " "+ (euler.z * 180 / Math.PI));
-				scene.appendChild(cone);
-			}
-		}
-		if(!showSpeed){
-			lineDOMObject.setAttribute("line", "path", path);
-			scene.appendChild(lineDOMObject);
-		}
-		console.log("Done adding to DOM.");
-	}
-	
-
 	//start reading file
-	var listOfDPs = new Array();
+	listOfDPs = new Array();
 	var pathToReplay = "/data/"+taskId+"_trial"+trialId+".replay";
 	var linesOfDataFile ="";
 	var rawFile = new XMLHttpRequest();
 	rawFile.open("GET", pathToReplay, false);
 	rawFile.onreadystatechange = function (){
 	    if(rawFile.readyState === 4 && (rawFile.status === 200 || rawFile.status == 0)){
-			finishedLoading(rawFile.responseText)
+			finishedLoading(taskId, trialId, color, rawFile.responseText)
 	    }
 	}
 	rawFile.send(null);
+}
+
+//when file is loaded, parse it
+function finishedLoading(taskId, trialId, color, text) {
+	lines = text.split('\n');
+	var lastDP = null;
+	var maxDistance = 0;
+	var maxDt = 0;
+	var maxDv = 0;
+
+	var duplicatesCount = 0;
+	for (var i = 0; i < lines.length; i++) {
+		var line = lines[i];
+		if (line.startsWith("t:")) {
+			//create new datapoint with timestamp and matrix
+			dp = new Datapoint();
+			dp.timestamp = parseFloat(line.substring(2, line.length));
+			i += 1; //skip next line
+			var matrix = new THREE.Matrix4();
+			matrix.set(
+				parseFloat(lines[i].substring(1, 6)),
+				parseFloat(lines[i].substring(7, 12)),
+				parseFloat(lines[i].substring(13, 18)),
+				parseFloat(lines[i].substring(19, 24)),
+				parseFloat(lines[i + 1].substring(1, 6)),
+				parseFloat(lines[i + 1].substring(7, 12)),
+				parseFloat(lines[i + 1].substring(13, 18)),
+				parseFloat(lines[i + 1].substring(19, 24)),
+				parseFloat(lines[i + 2].substring(1, 6)),
+				parseFloat(lines[i + 2].substring(7, 12)),
+				parseFloat(lines[i + 2].substring(13, 18)),
+				parseFloat(lines[i + 2].substring(19, 24)),
+				parseFloat(lines[i + 3].substring(1, 6)),
+				parseFloat(lines[i + 3].substring(7, 12)),
+				parseFloat(lines[i + 3].substring(13, 18)),
+				parseFloat(lines[i + 3].substring(19, 24))
+			);
+
+
+			//var quaternion = new THREE.Quaternion();
+			//var scale = new THREE.Vector3();
+			//matrix.decompose( dp.position, quaternion, scale );
+
+			dp.position = new THREE.Vector3(1, 1, 1).applyMatrix4(matrix);
+			//shift data to view, then add to path
+			//dp.position.applyQuaternion(quaternion);
+			dp.position.multiplyScalar(scaling);
+			dp.position.add(sceneOffset);
+
+			//current one is duplicate
+			if (listOfDPs.length > 0 && dp.position.equals(listOfDPs[listOfDPs.length-1].position))  {
+				duplicatesCount++;
+				//current one is the last one
+				listOfDPs[listOfDPs.length-1].timestamp = dp.timestamp;//refrehs timestap to last know timestamp at this pos
+				dp = listOfDPs[listOfDPs.length-1];
+			} else {
+				listOfDPs.push(dp);
+			}
+			
+			//calcualte distance of last DP to the current one
+			if (showSpeed && lastDP != null) {
+				lastDP.distanceToNext = lastDP.position.distanceTo(dp.position);
+				var dt = dp.timestamp - lastDP.timestamp;
+				var dv = lastDP.distanceToNext / dt;
+				if (lastDP.distanceToNext > maxDistance)
+					maxDistance = lastDP.distanceToNext;
+				if (dt > maxDt)
+					maxDt = dt;
+				if (dv > maxDv)
+					maxDv = dv;
+			}
+			lastDP = dp;
+			
+			i += 4; //skip rest
+		}
+	}
+
+	console.log("Done parsing to "+ listOfDPs.length + " elements. Max dv:"+maxDv+" maxDistance: "+maxDistance+" max dt: "+ maxDt+"s. Removed "+duplicatesCount+" duplicate points.");
+	
+	//add to DOM
+	if (!showSpeed){
+		//add one big line
+		var lineDOMObject = document.createElement("a-entity");
+		lineDOMObject.setAttribute("class",taskId+"-"+trialId)
+		lineDOMObject.setAttribute("line","color:"+color+";")
+		var path = new Array();
+	}
+	
+	var up = new THREE.Vector3(0,1,0);
+	
+	//add every position
+	for (var i = 0; i < listOfDPs.length; i++) {
+		if (showSpeed) {
+			if (i < listOfDPs.length-1) {
+				var line = document.createElement("a-entity");
+				line.setAttribute("class", taskId+"-"+trialId)
+			
+				//var hexBrightness = new Buffer(1/distance, 'hex')[0];
+				var dt = listOfDPs[i+1].timestamp - listOfDPs[i].timestamp;
+				var dv = listOfDPs[i].distanceToNext / dt;
+				var hexBrightness = (parseInt(255*dv / maxDv)).toString(16);
+				line.setAttribute("line", "color:#"+hexBrightness+hexBrightness+hexBrightness+";");
+				if (dt > 2*maxDt / 3){
+					//var hexBrightness = (parseInt(255*dv/maxDv)).toString(16);
+					line.setAttribute("line","color:#ff0000;");
+				}
+				line.setAttribute("line", "path", [listOfDPs[i].position,listOfDPs[i+1].position]);
+				scene.appendChild(line);
+			}
+		} else {
+			path.push(listOfDPs[i].position);
+		}
+		
+		//add cones for direction
+		if (showDir && i % 3 == 0 && i < listOfDPs.length-1) { //skip some elements
+			//add cone for direction
+			var cone = document.createElement("a-cone");
+			//cone.setAttribute("geometry","primitive","cone");
+			cone.setAttribute("class", taskId+"-"+trialId)
+			cone.setAttribute("color", color);
+			cone.setAttribute("radius-bottom", 0.008);
+			cone.setAttribute("radius-top", 0);
+			cone.setAttribute("open-ended", false);
+			cone.setAttribute("geometry", "segmentsRadial", 4);
+			cone.setAttribute("geometry", "segmentsHeight", 4);
+			cone.setAttribute("height", 0.04);
+			cone.setAttribute("position", listOfDPs[i].position);
+
+			var norm = new THREE.Vector3().copy(listOfDPs[i + 1].position).sub(listOfDPs[i].position).normalize();
+			//var norm = new THREE.Vector3(1,1,0).normalize();
+			var euler = new THREE.Euler(0, 0, 0, 'XYZ');
+			euler.setFromQuaternion(new THREE.Quaternion().setFromUnitVectors ( up, norm));
+		
+			cone.setAttribute("rotation", (euler.x * 180 / Math.PI) + " " + (euler.y * 180 / Math.PI) + " "+ (euler.z * 180 / Math.PI));
+			scene.appendChild(cone);
+		}
+	}
+	if(!showSpeed){
+		lineDOMObject.setAttribute("line", "path", path);
+		scene.appendChild(lineDOMObject);
+	}
+	console.log("Done adding to DOM.");
+	currentAnimStep=0;
+	nextAnimationStep();
+}
+
+function nextAnimationStep(){
+	currentAnimStep+=1;
+	//remove last animation
+	var cursorNode = document.getElementById("cursor");
+	cursorNode.removeChild(cursorNode.firstChild);
+	
+	var anim = document.createElement("a-animation");
+	anim.setAttribute("attribute", "position");
+	anim.setAttribute("from", AFRAME.utils.coordinates.stringify(listOfDPs[currentAnimStep].position));
+	anim.setAttribute("to", listOfDPs[currentAnimStep+1].position.x + " " + listOfDPs[currentAnimStep+1].position.y + " " + listOfDPs[currentAnimStep+1].position.z);
+	anim.setAttribute("dur", listOfDPs[currentAnimStep+1].timestamp-listOfDPs[currentAnimStep].timestamp);
+	cursorNode.appendChild(anim);
 }
 
 addTargetsToDOM();
